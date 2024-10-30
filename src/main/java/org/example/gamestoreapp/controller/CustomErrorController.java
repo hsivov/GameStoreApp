0b3fbd430ebe50f7f14ff8917d7a744d6d76b52e
hsivov <hsivov@students.softuni.bg>
2024-10-30 13:30:09 +0200

    Customizing 500 error page

--- src/main/java/org/example/gamestoreapp/controller/CustomErrorController.java
+++ src/main/java/org/example/gamestoreapp/controller/CustomErrorController.java
@@ -1,17 +1,34 @@
 package org.example.gamestoreapp.controller;
 
-import jakarta.servlet.RequestDispatcher;
-import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.boot.web.servlet.error.ErrorAttributes;
+import org.springframework.boot.web.error.ErrorAttributeOptions;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Map;
 
 @Controller
 public class CustomErrorController implements ErrorController {
 
+    private final ErrorAttributes errorAttributes;
+
+    public CustomErrorController(ErrorAttributes errorAttributes) {
+        this.errorAttributes = errorAttributes;
+    }
+
     @RequestMapping("/error")
-    public String handleError(HttpServletRequest request) {
-        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+    public String handleError(WebRequest webRequest, Model model) {
+        // Retrieve error attributes, including exception details
+        Map<String, Object> errorAttributes = this.errorAttributes.getErrorAttributes(webRequest,
+                ErrorAttributeOptions.of(ErrorAttributeOptions.Include.STACK_TRACE));
+
+        // Add attributes to the model to display them on the error page
+        model.addAttribute("errorAttributes", errorAttributes);
+
+        Object status = errorAttributes.get("status");
         int statusCode = status != null ? Integer.parseInt(status.toString()) : 500;
 
         if (statusCode == 401) {
